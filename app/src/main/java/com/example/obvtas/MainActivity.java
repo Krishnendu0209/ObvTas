7e38c340870ec8eaa90650fb15cdb22d50075ab3
Krishnendu Mukherjee <krishnendu0209@gmail.com>
2020-01-23 22:37:46 +0530

    Sixth COmmit -- Added the single grid view item image_grid.xml design (Basic), added the grid view in the activity_main.xml file, in Manifest added Internet permission and in the MainActivity.java file added three arraylists and have given the array adapter call

--- app/src/main/java/com/example/obvtas/MainActivity.java
+++ app/src/main/java/com/example/obvtas/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.obvtas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
 
 import com.example.obvtas.model.ImageDetails;
 
@@ -12,23 +13,29 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity
 {
     private String imageDetailsJSONString;
-    private ArrayList <ImageDetails> imageDetailsList= new ArrayList<>();
+    private ArrayList<ImageDetails> imageDetailsList = new ArrayList<>(); //All details will be added
+    private ArrayList<String> imageLinks = new ArrayList<>(); // All image links will be stored in the list
+    private ArrayList<String> imageTitles = new ArrayList<>(); // All image titles will be stored in the list
+    private GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageDetailsJSONString = loadJSONFromAsset();
-        if (!imageDetailsJSONString.equals("")) //If the loadJSONFromAsset does not return null then start parsing the data stored in the string variable
+        if(!imageDetailsJSONString.equals("")) //If the loadJSONFromAsset does not return null then start parsing the data stored in the string variable
         {
             parseStringJSONData(); //The parsing will begin
         }
+        ImageAdapter adapter = new ImageAdapter(getApplicationContext(),imageTitles,imageLinks); //Adapter has been instantiated with necessary parameters
+        gridView =findViewById(R.id.grid_view); //Grid view initialised
+        gridView.setAdapter(adapter); // The image adapter is called to get set with the data
     }
+
     /* The function is responsible for reading the data from the JSON file
         and storing it as string for further parsing and processing of the data
     */
@@ -43,14 +50,14 @@ public class MainActivity extends AppCompatActivity
             is.read(buffer); //All the data read from the file is stored in the buffer array
             is.close(); //Reading the JSON file is done, close input stream
             json = new String(buffer, "UTF-8"); //Store the buffer array data to the variable
-        }
-        catch(IOException ex)
+        } catch(IOException ex)
         {
             ex.printStackTrace();
             return null; //If any exception occurred then return null
         }
         return json; //If no exception then return the string that store the data.
     }
+
     /*
         The function is responsible for parsing the JSON array data stored in the string variable
     */
@@ -71,10 +78,11 @@ public class MainActivity extends AppCompatActivity
                 imageDetails.setServiceVersion(imageObject.getString("service_version")); //Get the service version
                 imageDetails.setTitle(imageObject.getString("title"));//Get the title
                 imageDetails.setUrl(imageObject.getString("url")); //Get the url
+                imageLinks.add(imageObject.getString("url"));// The array list will finally contain all the links of the images
                 imageDetailsList.add(imageDetails); //All the images' details are added as objects are added one by one to the list
+                imageTitles.add(imageObject.getString("title"));
             }
-        }
-        catch(JSONException e)
+        } catch(JSONException e)
         {
             e.printStackTrace();
         }
