d8fdb82db2f1bf845c02303e6b3b07a1d67951ba
Krishnendu Mukherjee <krishnendu0209@gmail.com>
2020-01-23 18:20:05 +0530

    Fourth Commit : Added the fucntion to handle JSON data loading from the file and storing it as a string for further parsing

--- app/src/main/java/com/example/obvtas/MainActivity.java
+++ app/src/main/java/com/example/obvtas/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.obvtas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
 public class MainActivity extends AppCompatActivity
 {
 
@@ -11,5 +15,26 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadJSONFromAsset();
+    }
+
+    public String loadJSONFromAsset()
+    {
+        String json = null;
+        try
+        {
+            InputStream is = getAssets().open("nasa_data.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer); //All the data read from the file is stored in the buffer array
+            is.close(); //Reading the JSON file is done, close input stream
+            json = new String(buffer, "UTF-8"); //Store the buffer array data to the variable
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+            return null; //If any exception occurred then return null
+        }
+        return json; //If no exception then return the string that store the data.
     }
 }
